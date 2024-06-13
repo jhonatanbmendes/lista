86f105bf7e727a4a4251eee11fe38b30ba7ec48b
Jhonata (trabalho) <jhonatanbmendes@gmail.com>
2024-06-13 17:47:07 -0400

    listando com sucesso

--- contatos.js
+++ contatos.js
@@ -16,9 +16,9 @@ lista = [{
 
 for (let i in lista) {
     console.log(lista[i].escola);
-    let itemTag = document.createElement('div');
+    // let itemTag = document.createElement('div');
     
-    document.getElementById('itemLista').innerHTML = `<div class="escola">`+lista[i].escola+`</div>
+    document.getElementById('itemLista').innerHTML += `<section class="item"><div class="escola">`+lista[i].escola+`</div>
     <div class="nomes" id="nome01">`+lista[i].nome01+`</div>
     <div class="nomes" id="tel01">
         <div>95 `+lista[i].tel01+`</div>
@@ -29,6 +29,6 @@ for (let i in lista) {
         `<div class="nomes" id="nome01">`+lista[i].nome02+`</div>
         <div class="nomes" id="tel02">
             <div>95 `+lista[i].tel02+`</div>
-            <a href="https://wa.me/5595`+lista[i].tel02+`?text=Olá `+lista[i].nome02+`"><img src="whats.png" alt="whatsapp"></a>`
+            <a href="https://wa.me/5595`+lista[i].tel02+`?text=Olá `+lista[i].nome02+`"><img src="whats.png" alt="whatsapp"></a></div>`
     };
 }
