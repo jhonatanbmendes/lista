30c9fab8874488ec7e65cec06cd209385ad5e470
Jhonatan Barbosa Mendes <jhonatanbmendes@gmail.com>
2024-06-13 21:52:22 -0400

    Foi adicionado os nomes das escolas.

--- contatos.js
+++ contatos.js
@@ -1,43 +1,451 @@
 lista = [{
-    escola: "Escola Municipal Palmira de Castro Machado",
-    nome01: "Antonia Araújo",
-    tel01: "991154856",
-    nome02: "Jhonatan Mendes",
-    tel02: "991458485"
-},
-{
-    escola: "",
+    escola: "Escola Municipal Aquilino da Mota Duarte",
     nome01: "",
     tel01: "",
     nome02: "",
     tel02: ""
 },
 {
-    escola: "Escola Municipal Ioládio Batista",
-    nome01: "João Almeida",
-    tel01: "991154856",
+    escola: "Escola Municipal Arco-Íris",
+    nome01: "",
+    tel01: "",
     nome02: "",
-    tel02: "991458485"
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Branca de Neve",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Cantinho do Céu",
+    nome01: "Rosimeire Alves",
+    tel01: "991300196",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Cantinho Feliz",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Centenário de Boa Vista",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Criança Feliz",
+    nome01: "Sheila Sheron",
+    tel01: "991280736",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Cunhatã Curumin",
+    nome01: "Renan Rocha",
+    tel01: "991581030",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Dalício Farias Filho",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Delacir de Melo Lima",
+    nome01: "Francisca Alexsandra",
+    tel01: "991479954",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Doutor Sílvio Leite",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Estrelinha Mágica",
+    nome01: "Zinete Delfino",
+    tel01: "991209362",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Francisco Cássio de Moraes",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Francisco de Souza Bríglia",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Frei Artur Agostine",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Franco de Souza Hilda",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Ioládio Batista da Silva",
+    nome01: "Carla Sarmento",
+    tel01: "991159645",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Isete Evangelista Albuquerque",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Jael da Silva Barradas",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Jânio da Silva Quadros",
+    nome01: "Ana Paula",
+    tel01: "984017671",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Jardim das Copaíbas",
+    nome01: "Geovani Ramos",
+    tel01: "981189883",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Jóquei Clube",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal José Arnóbio da Silva",
+    nome01: "Queila Alves",
+    tel01: "991338003",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Juslany de Souza Flores",
+    nome01: "Alana Karen",
+    tel01: "981094829",
+    nome02: "Marcos Melo",
+    tel02: "981115567"
+},
+{
+    escola: "Escola Municipal Laucides Inácio de Oliveira",
+    nome01: "Genivilce Maita",
+    tel01: "991138202",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Luiz Canará",
+    nome01: "Ellem Naiane",
+    tel01: "991559578",
+    nome02: "Samara Nascimento",
+    tel02: "981003081"
+},
+{
+    escola: "Escola Municipal Maria de Fátima Faria Andrade",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Maria Gonçalves Vieira",
+    nome01: "Jeanne Lopes",
+    tel01: "991265389",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Maria Teresa Maciel da Silveira Melo",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Martinha Thury Vieira",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Menino Jesus",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Menino Jesus de Praga",
+    nome01: "Nirlley Lira",
+    tel01: "984047564",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Newton Tavares",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Canaã Nova",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Palmira de Castro Machado",
+    nome01: "Antonia Araújo",
+    tel01: "991180551",
+    nome02: "Jhonatan Mendes",
+    tel02: "988032944"
+},
+{
+    escola: "Escola Municipal Pequeno Polegar",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Pequeno Príncipe",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Pingo de Gente",
+    nome01: "Maria da Conceição",
+    tel01: "991408781",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professor Carlos Raimundo Rodrigues",
+    nome01: "Jancicleide Ortha",
+    tel01: "984093122",
+    nome02: "Márcia Maciel",
+    tel02: "991135486"
+},
+{
+    escola: "Escola Municipal Professora Amazona de Oliveira Monteiro",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Ana Sandra Nascimento Queiroz",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Antônia Fernandes Cutrim",
+    nome01: "Valdenira Correa",
+    tel01: "991147042",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Carmen Eugênia Macaggi",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Danúbia Carvalho de Oliveira",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Edsonina de Barros Villa",
+    nome01: "Fábio Oliveira",
+    tel01: "991389715",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Glemíria Gonzaga Andrade",
+    nome01: "Aurilene Anjos",
+    tel01: "991356090",
+    nome02: "Daniele Lima",
+    tel02: "991256982"
+},
+{
+    escola: "Escola Municipal Professora Ivany dos Santos Parente",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Maria Francisca da Silva Lemos",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Professora Maria Gertrudes Mota de Lima",
+    nome01: "Francivânia Brandão",
+    tel01: "984039341",
+    nome02: "Eriane Cerdeira",
+    tel02: "991448116"
+},
+{
+    escola: "Escola Municipal Raimundo Eloy Gomes",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Raio de Sol",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Rujane Severiano dos Santos",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Senador Darcy Ribeiro",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Sonho Infantil",
+    nome01: "Alzira Rabelo",
+    tel01: "991399749",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Tia Linda",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Valdemarina Normando Martins",
+    nome01: "Everaldo",
+    tel01: "991411913",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Vovó Clara",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Vovô Dandãe",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Vovó Eurídes",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Vovó Júlia",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Zacarias Assunção Ribeiro Araújo",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
 }];
 
-lista = lista.
-console.log(lista);
 
 for (let i in lista) {
 
-    let aux = `<section class="item"><div class="escola">` + lista[i].escola + `</div>
-    <div class="nomes" id="nome01">`+ lista[i].nome01 + `</div>
-    <div class="nomes" id="tel01">
-        <a href="https://wa.me/5595`+ lista[i].tel01 + `?text=Olá ` + lista[i].nome01 + `"><div>95 ` + lista[i].tel01 + `</div><img src="whats.png" alt="whatsapp"></a>
-    </div>`;
-
-    if (lista[i].nome02 !== "") {
-        aux += `<div class="nomes" id="nome01">` + lista[i].nome02 + `</div>
-        <div class="nomes" id="tel02">
-            <a href="https://wa.me/5595`+ lista[i].tel02 + `?text=Olá ` + lista[i].nome02 + `"><div>95 ` + lista[i].tel02 + `</div><img src="whats.png" alt="whatsapp"></a>
+    if(lista[i].nome01 !== ""){
+        
+        let aux = `<section class="item"><div class="escola">` + lista[i].escola + `</div>
+        <div class="nomes" id="nome01">`+ lista[i].nome01 + `</div>
+        <div class="nomes" id="tel01">
+            <a href="https://wa.me/5595`+ lista[i].tel01 + `?text=Olá ` + lista[i].nome01 + `"><div>95 ` + lista[i].tel01 + `</div><img src="whats.png" alt="whatsapp"></a>
         </div>`;
-    };
+    
+        if (lista[i].nome02 !== "") {
+            aux += `<div class="nomes" id="nome01">` + lista[i].nome02 + `</div>
+            <div class="nomes" id="tel02">
+                <a href="https://wa.me/5595`+ lista[i].tel02 + `?text=Olá ` + lista[i].nome02 + `"><div>95 ` + lista[i].tel02 + `</div><img src="whats.png" alt="whatsapp"></a>
+            </div>`;
+        };
+    
+        document.getElementById('itemLista').innerHTML += aux + '</div>';
+    }
 
-    document.getElementById('itemLista').innerHTML += aux + '</div>';
 
 }
