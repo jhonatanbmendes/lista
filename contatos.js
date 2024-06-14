cd7c4c2ce5267235e5daf0d7871bb0fe1203b229
Jhonatan Barbosa Mendes <jhonatanbmendes@gmail.com>
2024-06-13 20:00:06 -0400

    Sistema responsivo e com link no numero do telefone

--- contatos.js
+++ contatos.js
@@ -6,32 +6,38 @@ lista = [{
     tel02: "991458485"
 },
 {
-    escola: "Escola Municipal Ivanyr Parente",
+    escola: "",
+    nome01: "",
+    tel01: "",
+    nome02: "",
+    tel02: ""
+},
+{
+    escola: "Escola Municipal Ioládio Batista",
     nome01: "João Almeida",
     tel01: "991154856",
     nome02: "",
     tel02: "991458485"
 }];
 
+lista = lista.
+console.log(lista);
 
 for (let i in lista) {
-    // console.log(lista[i].escola);
-    
-    let aux = `<section class="item"><div class="escola">`+lista[i].escola+`</div>
-    <div class="nomes" id="nome01">`+lista[i].nome01+`</div>
+
+    let aux = `<section class="item"><div class="escola">` + lista[i].escola + `</div>
+    <div class="nomes" id="nome01">`+ lista[i].nome01 + `</div>
     <div class="nomes" id="tel01">
-        <div>95 `+lista[i].tel01+`</div>
-        <a href="https://wa.me/5595`+lista[i].tel01+`?text=Olá `+lista[i].nome01+`"><img src="whats.png" alt="whatsapp"></a>
+        <a href="https://wa.me/5595`+ lista[i].tel01 + `?text=Olá ` + lista[i].nome01 + `"><div>95 ` + lista[i].tel01 + `</div><img src="whats.png" alt="whatsapp"></a>
     </div>`;
 
-    if(lista[i].nome02 !== ""){
-        aux += `<div class="nomes" id="nome01">`+lista[i].nome02+`</div>
+    if (lista[i].nome02 !== "") {
+        aux += `<div class="nomes" id="nome01">` + lista[i].nome02 + `</div>
         <div class="nomes" id="tel02">
-            <div>95 `+lista[i].tel02+`</div>
-            <a href="https://wa.me/5595`+lista[i].tel02+`?text=Olá `+lista[i].nome02+`"><img src="whats.png" alt="whatsapp"></a>
+            <a href="https://wa.me/5595`+ lista[i].tel02 + `?text=Olá ` + lista[i].nome02 + `"><div>95 ` + lista[i].tel02 + `</div><img src="whats.png" alt="whatsapp"></a>
         </div>`;
     };
 
-    document.getElementById('itemLista').innerHTML += aux+'</div>';
+    document.getElementById('itemLista').innerHTML += aux + '</div>';
 
 }
